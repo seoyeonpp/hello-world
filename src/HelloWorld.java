45de60e2de57d50970b71b9c68faf615948c451f
seoyeonpp <seoyeonpub@gmail.com>
2021-11-14 22:03:38 +0900

    Good Bye 메세지 추가

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
 	public static void main(String[] orgs) {
 		System.out.println("Hello World");
 		System.out.println("Hi world");
+		System.out.println("Good Bye");
 	}
 }
